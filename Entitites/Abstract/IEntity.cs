0264937ea49561bcaa4aae6d665f8786045c68f3
Dorukhan Cosgun <66283837+dorukhancosgun@users.noreply.github.com>
2021-02-07 01:34:10 +0100

    Add project files.

---
+++ Entitites/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitites.Abstract
+{
+    //IEntity implament eden class bir veritabani tablosudur.
+    public interface IEntity
+    {
+    }
+}
